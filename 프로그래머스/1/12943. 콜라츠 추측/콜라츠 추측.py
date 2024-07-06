35ddef749b43769aaf727bb893233b46b88c9c9a
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-07 01:02:16 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,12 +1,16 @@
 def solution(num):
+    # 주어진 수가 1인 경우에 0 반환
     if num == 1:
         return 0
     
     cnt = 0
     
+    # 주어진 수가 1이 될 때 까지 작업 반복
     while num !=1: 
+        # 만일 작업을 500번 반복했다면 -1 반환
         if cnt == 500:
             return -1
+        
         if num % 2 == 0:
             num /= 2
             cnt += 1
@@ -15,4 +19,5 @@ def solution(num):
             cnt += 1
     
     answer = cnt
+    
     return answer
