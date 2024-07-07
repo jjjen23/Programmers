2f8f3dac5244a02de067575059fb6f7abb1db2ac
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-08 00:29:23 +0900

    [level 1] Title: 행렬의 덧셈, Time: 38.70 ms, Memory: 23 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,7 +1,9 @@
 def solution(arr1, arr2):
-    rows = len(arr1)
-    colums = len(arr1[0])
+    rows = len(arr1) # 2차원 행 수 구하기
+    colums = len(arr1[0]) # 2차원 열 수 구하기
+    
     answer = [[0 for i in range(colums)] for i in range(rows)]
+    
     for i in range(rows):
         for j in range(colums):
             answer[i][j] = arr1[i][j] + arr2[i][j]
