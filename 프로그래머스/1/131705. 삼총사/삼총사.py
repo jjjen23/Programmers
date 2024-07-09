02e83641d5b3f2987262e7b6df859a68408a2e25
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-09 23:29:08 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -0,0 +1,12 @@
+# 조합 모듈 사용
+from itertools import combinations
+
+def solution(number):
+    answer = 0
+    
+    triples = list(combinations(number, 3))
+    
+    for triple in triples:
+        if sum(triple) == 0:
+            answer += 1
+    return answer
