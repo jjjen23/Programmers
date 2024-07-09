f773e298424d904e38a1ba03ed447115accc86f9
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-10 00:51:52 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,9 +1,14 @@
 def solution(array, commands):
     answer = []
+    
     for arr in commands:
         tem = []
+        #인덱스 슬라이싱
         for i in range(arr[0]-1, arr[1]):
             tem.append(array[i])
+        #슬라이싱한 리스트 정렬    
         tem.sort()
+        #원하는 원소값 정답 리스트에 추가
         answer.append(tem[arr[2]-1])
+        
     return answer
