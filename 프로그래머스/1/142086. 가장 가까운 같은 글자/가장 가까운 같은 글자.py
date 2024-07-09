ec91590d43e0eca92d06c0512353f6158bf33b93
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-10 00:45:01 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.79 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -3,9 +3,11 @@ def solution(s):
     dict = {}
     
     for i in range(len(s)):
+        # 처음 나오는 알파벳은 정답 리스트에 -1추가하고, 사전에 인덱스 번호 등록하기
         if s[i] not in dict:
             answer.append(-1)
             dict[s[i]] = i
+        # 처음 나오는 알파벳이 아니라면 정답리스트에 (현재 인덱스 번호 - 사전에 저장했던 인덱스번호)추가해주고 현재 인덱스 번호로 갱신해주기     
         else:
             answer.append(i - dict[s[i]])
             dict[s[i]] = i
