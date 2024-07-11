7b918637d46751d999afe56f6114f1ed978c2b80
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-12 00:33:44 +0900

    [level 1] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -4,9 +4,9 @@ def solution(a, b, n):
     # update 해야하는 거 : n 
     # 내가 가진 빈병이 a보다 작으면 종료(더 이상 교환 불가)
     while n >= a:
-        reminder = n % a
+        remainder = n % a
         exchange = int(n//a) * b
         answer += exchange
-        n = exchange + reminder
+        n = exchange + remainder
         
     return answer
