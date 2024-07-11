2e8c3c459a2a65bb59c339b53f72c19670ccbf8e
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-11 23:27:06 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,15 +1,12 @@
 def solution(food):
     answer = ''
-    tem_list = []
+    right = ''
     for i in range(1,len(food)):
         tem = int(food[i] // 2)
-        for j in range(tem):
-            tem_list.append(i)
+        right += str(i) * tem
     
-    left = ''.join(map(str,tem_list))
-    tem_list.sort(reverse=True)
-    right = ''.join(map(str,tem_list))
-    
-    answer = left + '0' + right
+    left = right[::-1]
+    answer+=right+'0'+left
+
             
     return answer
