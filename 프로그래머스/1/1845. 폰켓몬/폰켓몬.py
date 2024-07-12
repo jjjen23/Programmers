020c7c2ffdbcbe3dbb2088639d16da7d6619bdbe
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-13 00:22:01 +0900

    [level 1] Title: 폰켓몬, Time: 0.82 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,8 +1,12 @@
 def solution(nums):
+    # 선택가능한 최대 종의 수가 답
     answer = 0
     nMari = len(nums) // 2
     nums = set(nums)
     
+    # 집합을 이용해서 종의 수를 파악한다.
+    
+    # 만일 종의 수가 선택가능한 마리보다 작다면, 종의 수가 답(최대 종의 수니까.)
     if nMari > len(nums):
         answer = len(nums)
     else: answer = nMari
