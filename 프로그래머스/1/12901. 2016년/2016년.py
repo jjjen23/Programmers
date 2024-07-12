205c45fb1d5768663be327e180faf3a0b9c8dccd
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-13 00:34:13 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,6 +1,7 @@
 import calendar
 
 def solution(a, b):
+    # 0은 월요일
     yoil = calendar.weekday(2016,a,b)
     yoil_list = ["MON","TUE","WED","THU","FRI","SAT","SUN"]
     answer = yoil_list[yoil]
