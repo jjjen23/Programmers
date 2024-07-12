614e6007d2bdd5e02ee685b11564b934d01885cd
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-12 22:59:08 +0900

    [level 1] Title: 명예의 전당 (1), Time: 3.93 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -2,16 +2,15 @@ def solution(k, score):
     answer = []
     award_list = []
     for i in range(len(score)):
+        
         if len(award_list) < k :
             award_list.append(score[i])
             answer.append(min(award_list))
         else:
-            minValue = min(award_list)
-            if  minValue < score[i] :
-                indexNum = award_list.index(minValue)
-                award_list[indexNum] = score[i]
-                answer.append(min(award_list))
-            else:
-                answer.append(min(award_list))
+            if  min(award_list) < score[i] :
+                award_list.remove(min(award_list))
+                award_list.append(score[i])
+            
+            answer.append(min(award_list))
         
     return answer
