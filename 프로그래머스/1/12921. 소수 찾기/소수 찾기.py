8f87be25b64d2b7976c9a454f65c4a880cd30802
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-17 01:13:04 +0900

    [level 1] Title: 소수 찾기, Time: 330.52 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,17 @@
+def solution(n):
+    array = [True for i in range(n+1)]
+    answer = 0
+    # 이게 진짜 에라토스테네스의 체.. 아까 그건 복잡도만 줄인 것.
+    # 에라토스테네스의 체 알고리즘은 여러개의 소수를 구하는 문제(위 문제)에 적합
+    for i in range(2, int(n**0.5) + 1):
+        if array[i] == True:
+            # i를 제외한 모든 배수 지우기(고로 j는 2부터 시작함)
+            j = 2
+            while i * j <= n:
+                array[i*j] = False
+                j += 1
+    
+    for i in range(2, n+1):
+        if array[i] == True:
+            answer += 1
+    return answer
