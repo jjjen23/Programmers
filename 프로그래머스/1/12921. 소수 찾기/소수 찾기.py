214b286f5f3d416ec5fe3ead02e8c19ee03410dd
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-17 01:27:23 +0900

    [level 1] Title: 소수 찾기, Time: 260.87 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,19 +1,9 @@
 """
-# 에라토스테네스의 체 간단하고 간지버전...
-def solution(n):
-    num=set(range(2,n+1))
-
-    for i in range(2,n+1):
-        if i in num:
-            num-=set(range(2*i,n+1,i))
-    return len(num)
-"""
-
+# 본래 답...
 def solution(n):
     array = [True for i in range(n+1)]
     answer = 0
-    # 이게 진짜 에라토스테네스의 체.. 아까 그건 복잡도만 줄인 것.
-    # 에라토스테네스의 체 알고리즘은 여러개의 소수를 구하는 문제(위 문제)에 적합
+
     for i in range(2, int(n**0.5) + 1):
         if array[i] == True:
             # i를 제외한 모든 배수 지우기(고로 j는 2부터 시작함)
@@ -22,10 +12,27 @@ def solution(n):
                 array[i*j] = False
                 j += 1
     
-    # for i in range(2, n+1):
-    #     if array[i] == True:
-    #         answer += 1
+    for i in range(2, n+1):
+         if array[i] == True:
+             answer += 1
     
-    answer = array.count(True) - 2 #0,1 제외
+    
+    return answer
+"""
+
+# 간단하고 효율적인 버전
+def solution(n):
+    
+    # 2부터 n까지의 집합 만들기
+    array = set(range(2,n+1))
+    
+    # 이게 진짜 에라토스테네스의 체.. 아까 그건 범위만 줄인 것.
+    # 에라토스테네스의 체 알고리즘은 범위 내에서 여러개의 소수를 구하는 문제(위 문제)에 적합
+    for i in range(2, int(n**0.5) + 1):
+        # 제거하지 않은 것 중 최소값
+        if i in array:
+            # 범위 내에서 i를 제외한 모든 i의 배수 제거
+            array -= set(range(i*2, n+1, i))
+    answer = len(array)
     
     return answer
