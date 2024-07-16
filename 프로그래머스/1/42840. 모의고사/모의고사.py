d07275576bec7931d6a4f87e25fd4f2014b6ec06
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-16 23:46:01 +0900

    [level 1] Title: 모의고사, Time: 5.79 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -14,17 +14,21 @@ def solution(answers):
             total2+=1
         if supoza3[i % len(supoza3)] == answers[i]:
             total3+=1
-        
+    
+    # 딕셔너리에 각 수포자의 총 점수 저장
     res = {1:total1, 2:total2, 3:total3}
     
+    # 가장 최고 점수를 저장해두기
     max_value = max(res.values())
     
     answer = []
     
+    # 최고 점수와 동일한 점수를 가진 수포자 번호를 정답 리스트에 저장
     for item in res.items():
         if item[1] == max_value:
             answer.append(item[0])
     
+    # 가장 높은 점수를 받은 사람이 여럿일 경우, 오름차순 정렬해주기
     answer.sort()
     
     
