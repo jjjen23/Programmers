4d6356ed615154ebc46287ec69cb668a357a3644
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-20 23:37:09 +0900

    [level 1] Title: 문자열 나누기, Time: 15.16 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -2,14 +2,16 @@ def solution(s):
     s = list(s)
     answer = 0
     
-    # 더이상 first와 비교될 대상이 없으면 종료하는 조건을 추가하기..!
+    # 문자열이 없을 때 까지 반복
     while len(s) > 0:
         first = s.pop(0)
         first_res = 1
         nexxt_res = 0
         tem_res = [first]
         
+        # 첫글자 카운트 수와 첫글자 제외 카운트 수가 동일할 때 까지 반복
         while first_res != nexxt_res:
+            # 더 이상 읽을 문자열이 없다면, 지금까지 읽은 문자열 분리 후, 종료
             if len(s) == 0:
                 break
             
@@ -20,6 +22,7 @@ def solution(s):
             elif first == nexxt:
                 first_res+=1
         print(tem_res)
+        # 두번째 while문 종료 -> 문자열 분리됨 의미, 정답에 1씩 더하기 
         answer += 1
         
     return answer
