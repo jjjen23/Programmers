a43c9bb063f12a1b517db3f32b0e36be689b2c78
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-21 01:00:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 47.25 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,6 @@
 # def solution(participant, completion):
     
-#     participant.sort()
-#     completion.sort()
-    
+    # 시간초과 : 해당 코드 이용시 시간복잡도 O(n*m)
 #     for ant in completion:
 #         if ant in participant:
 #             participant.remove(ant)
@@ -11,22 +9,27 @@
     
 #     return answer
 
+# 딕셔너리(해시맵)이용시 시간복잡도 O(n)
 def solution(participant, completion):
-    participant_dict = {}
     
-    # 참가자 목록을 딕셔너리에 추가
-    for person in participant:
-        if person in participant_dict:
-            participant_dict[person] += 1
+    partDict = {}
+    
+    for i in participant:
+        if i not in partDict:
+            partDict[i] = 1
+        # 동명이인의 경우 +1
         else:
-            participant_dict[person] = 1
+            partDict[i] += 1
     
-    # 완주자 목록을 딕셔너리에서 제거
-    for person in completion:
-        if person in participant_dict:
-            participant_dict[person] -= 1
+    # 완주선수 리스트에서 -1 해주기
+    for i in completion:
+        if i in partDict:
+            partDict[i] -= 1
     
-    # 남은 참가자 찾기
-    for person, count in participant_dict.items():
-        if count > 0:
-            return person
+    for part, cnt in partDict.items():
+        # 값이 0이 아닌 참가자가 완주하지 못한 참가자.(동명이인도 마찬가지) 
+        if cnt != 0:
+            answer = part
+            
+    return answer
+
