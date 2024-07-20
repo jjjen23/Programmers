f80958d8457ae4b4dee48921844f9f6700fbca7d
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-21 01:59:10 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,32 @@
+def solution(n, lost, reserve):
+    answer = 0
+    
+    answer = n - len(lost) # 일단 체육복이 있는 사람
+    
+    
+    # 여벌을 가져온 학생이 도난 당한 경우, 못빌려줘. -> 이걸 처리해야겟네..
+    n = set(lost) & set(reserve) # 교집합.
+    if len(n) != 0:
+        for i in n:
+            reserve.remove(i)
+            lost.remove(i)
+            answer += 1
+    
+    reserve.sort()
+    lost.sort()
+    
+    # 3중 하나라도 만족하면 체육복있는 사람이 됨
+    for i in lost:
+        # 바로 앞번호에게 빌린 경우    
+        if i-1 in reserve:
+            answer+=1
+            reserve.remove(i-1)
+            continue
+        
+        # 바로 뒷번호에게 빌린 경우
+        if i+1 in reserve:
+            answer+=1
+            reserve.remove(i+1)
+            continue
+            
+    return answer
