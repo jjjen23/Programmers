4b9f8219a74723cbde41886044dc1e4aef21f0e0
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-07-21 02:02:59 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -4,7 +4,7 @@ def solution(n, lost, reserve):
     answer = n - len(lost) # 일단 체육복이 있는 사람
     
     
-    # 여벌을 가져온 학생이 도난 당한 경우, 못빌려줘. -> 이걸 처리해야겟네..
+    # 여벌을 가져온 학생이 도난 당한 경우, 못빌려줘. 
     n = set(lost) & set(reserve) # 교집합.
     if len(n) != 0:
         for i in n:
@@ -12,10 +12,11 @@ def solution(n, lost, reserve):
             lost.remove(i)
             answer += 1
     
+    # 정렬 해야 정답됨.
     reserve.sort()
     lost.sort()
     
-    # 3중 하나라도 만족하면 체육복있는 사람이 됨
+    # 둘 중 하나라도 만족하면 체육복있는 사람이 됨(단, 하나의 if문만 해당해야 함 -> 각 if문에 continue꼭 추가해야함)
     for i in lost:
         # 바로 앞번호에게 빌린 경우    
         if i-1 in reserve:
