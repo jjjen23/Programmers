673c9bedb83cfe9f01fc9832fe909867e2aca895
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-08-02 00:07:59 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.32 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -48,3 +48,16 @@ def solution(wallpaper):
             break
     
     return answer
+
+"""
+
+def solution(wall):
+    a, b = [], []
+    for i in range(len(wall)):
+        for j in range(len(wall[i])):
+            if wall[i][j] == "#":
+                a.append(i)
+                b.append(j)
+    return [min(a), min(b), max(a) + 1, max(b) + 1]
+    
+"""
