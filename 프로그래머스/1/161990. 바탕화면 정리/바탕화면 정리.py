a567a06ebbc740234f9d89c249267723cdbdc09f
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-08-02 00:11:16 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.32 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,3 +1,5 @@
+# [lux = 열최소, luy = 행최소 rdx = 열최대, rdy = 행최대]
+
 def solution(wallpaper):
     answer = []
     row = len(wallpaper)
@@ -50,14 +52,15 @@ def solution(wallpaper):
     return answer
 
 """
+# 샵이 나오는 좌표 다 집어 넣고 최대 최소 구하기
 
 def solution(wall):
     a, b = [], []
     for i in range(len(wall)):
         for j in range(len(wall[i])):
             if wall[i][j] == "#":
-                a.append(i)
-                b.append(j)
+                a.append(i) # row 좌표 저장
+                b.append(j) # col 좌표 저장
     return [min(a), min(b), max(a) + 1, max(b) + 1]
     
 """
