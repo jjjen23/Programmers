0bdd00a04215738f91216ff9bb7284ad778d38af
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-08-03 15:02:30 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.44 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -0,0 +1,52 @@
+def solution(today, terms, privacies):
+    answer = []
+    
+    dict1 = {}
+    
+    
+    for term in terms:
+        k, v = term.split(" ")
+        dict1[k] = int(v)
+    
+    for i in range(len(privacies)):
+        
+        nalja, alpa = privacies[i].split(" ")
+        year, month, day = map(int, nalja.split("."))
+        
+        newday = day - 1
+        
+        daycarry = 0
+        
+        if newday == 0 :
+            newday = 28
+            daycarry = -1
+            
+        newmonth = (month + dict1[alpa]) + daycarry
+        
+        mcarry = 0
+        if newmonth > 12 :
+            mcarry = newmonth // 12
+            newmonth = newmonth % 12
+        
+        ycarry = 0
+        if newmonth == 0 :
+            newmonth = 12
+            ycarry = -1 
+            
+        newyear = year + mcarry + ycarry
+        
+        #print(newyear, newmonth, newday)
+        
+        state = str(newyear) + f"{newmonth:02}" + f"{newday:02}"
+        
+        #print(state)
+        today = ''.join(today.split("."))
+        #print(today)
+        
+        # 파기할 정보 목록 
+        if int(today) > int(state):
+            answer.append(i+1)
+        
+
+        
+    return answer
