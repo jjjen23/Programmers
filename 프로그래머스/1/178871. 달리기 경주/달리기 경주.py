c6c378c43fbf41b5eef07497b21c07e3a3be2d5e
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-08-07 00:30:28 +0900

    [level 1] Title: 달리기 경주, Time: 1236.88 ms, Memory: 91.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -0,0 +1,28 @@
+# def solution(players, callings):
+#     #answer = []
+#     for name in callings:
+#         i = players.index(name)
+#         players[i], players[i-1] = players[i-1], players[i]
+        
+#     return players
+
+
+
+def solution(players, callings):
+    dict1 = {}
+    for i in range(len(players)):
+        dict1[players[i]] = i
+        
+    #answer = []
+    
+    for name in callings:
+        idx = dict1[name]
+        dict1[name] -=1
+        dict1[players[idx-1]] +=1
+        
+        players[idx-1], players[idx] =  players[idx], players[idx-1]
+    
+    
+    
+    
+    return players
