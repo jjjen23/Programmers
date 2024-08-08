8f6ea3a82f61e4d23f36e92d6ebf7a0c9de697a8
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-08-09 00:12:48 +0900

    [level 1] Title: 신고 결과 받기, Time: 198.95 ms, Memory: 45 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,27 +1,32 @@
 def solution(id_list, report, k):
     answer = []
     
+    # 유저 별로 신고한 사람을 저장할 딕셔너리(사람 별, 중복신고 막기 위함)
+    # 딕셔너리 안의 딕셔너리 구조로 자료구조 이용
     user_repo = {}
+    # 유저 별 신고 총합 횟수 저장 딕셔너리
     total = {}
     
     for name in id_list:
-        user_repo[name] = {}
-        total[name] = 0
+        user_repo[name] = {} # 빈 딕셔너리 생성(딕셔너리 안의 빈 딕셔너리)
+        total[name] = 0 # 0으로 초기화
     
     for re in report:
         name, who = re.split(" ")
+        # 이용자 딕셔너리에 신고자의 키값이 없다면 카운트 반영됨
         if who not in user_repo[name]:
-            total[who] += 1
-            user_repo[name][who] = True
+            total[who] += 1 
+            user_repo[name][who] = True # 신고자 키 추가해주기
     
     
     report_name = []
     
+    # K 번 이상 신고당하여 이용이 정지된 유저 리스트 만들어주기
     for name, value in total.items():
         if value >= k:
             report_name.append(name)
     
-    
+    # 이용자별로 신고한 사람중 이용이 정지된 사람이 있다면 카운트하여 정답 배열에 추가(= 유저가 받게될 메일의 수)
     for name in id_list:
         tem = 0
         for i in report_name:
