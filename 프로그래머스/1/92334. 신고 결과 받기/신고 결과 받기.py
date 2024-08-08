4eec1807761f1dba42aa630e3addbcaccb7aa1e0
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-08-09 00:00:16 +0900

    [level 1] Title: 신고 결과 받기, Time: 225.01 ms, Memory: 44.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -0,0 +1,33 @@
+def solution(id_list, report, k):
+    answer = []
+    
+    user_repo = {}
+    
+    total = {}
+    
+    for name in id_list:
+        user_repo[name] = {}
+        total[name] = 0
+    
+    for re in report:
+        name, who = re.split(" ")
+        if who not in user_repo[name]:
+            total[who] += 1
+            user_repo[name][who] = True
+    
+    
+    report_name = []
+    
+    for name, value in total.items():
+        if value >= k:
+            report_name.append(name)
+    
+    
+    for name in id_list:
+        tem = 0
+        for i in report_name:
+            if i in user_repo[name]:
+                tem +=1
+        answer.append(tem)
+        
+    return answer
