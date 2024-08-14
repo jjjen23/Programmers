44a3a644ee98155d1e7882528063b2b01b19aa68
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-08-14 16:58:13 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -2,11 +2,11 @@ def solution(n):
     answer = 0
     origin = n
     
-    # print(str(bin(origin)[2:]).count('1'))
+    #print(bin(origin)[2:].count('1'))
     # print(bin(origin)[2:])
     while True:
         n+=1
-        if str(bin(origin)[2:]).count('1') == str(bin(n)[2:]).count('1'):
+        if bin(origin)[2:].count('1') == bin(n)[2:].count('1'):
             answer = n
             break
     return answer
