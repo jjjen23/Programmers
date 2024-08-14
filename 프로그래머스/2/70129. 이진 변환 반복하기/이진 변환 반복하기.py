8cf236fd09b5e9b694b79d638d8d6c45af6c2c40
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-08-14 16:35:25 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.96 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,3 +1,5 @@
+# 어짜피 갱신에 필요한 것은 1의 갯수
+
 def solution(s):
     answer = []
     # 길이를 2진법으로 표현
@@ -6,8 +8,9 @@ def solution(s):
     while s != "1":
         zerocnt = s.count("0")
         delzerocnt += zerocnt
-        s = s.replace('0', '', zerocnt)
-        s = bin(len(s))[2:]
+        #s = s.replace('0', '', zerocnt)
+        onecnt=s.count("1")
+        s = bin(onecnt)[2:]
         bincnt += 1
     
     # print(s)
