6fcbab9b0ba922ddbda2e1e43f2c25d842f993c2
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-08-14 23:02:24 +0900

    [level 2] Title: 숫자의 표현, Time: 8.36 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,15 @@
+def solution(n):
+    answer = 0
+    numlist = list(range(1,n+1))
+    print(numlist)
+    
+    for i in range(len(numlist)):
+        target = 0
+        for j in range(i,len(numlist)):
+            target += numlist[j]
+            if target == n:
+                answer += 1
+                break
+            elif target > n:
+                break
+    return answer
