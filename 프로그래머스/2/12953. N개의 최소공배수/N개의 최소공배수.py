d7d8453f2e7217e81e03f18245b95c092e7483a1
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-08-19 01:44:39 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,6 +1,8 @@
 def solution(arr):
     answer = 0
     
+    # 세 수 a, b, c의 최소공배수 = 두 수 a, b의 최소공배수인 l과 c의 최소공배수 lcm
+    # arr의 요소가 2 이상일 때까지 반복
     while len(arr)  > 1:
         
         x_orign = arr.pop()
@@ -8,11 +10,14 @@ def solution(arr):
         
         x, y = x_orign, y_orign
         
+        # gcd 구하기
         while y:
             x, y = y, x % y
-            
+        
+        # lcm 추가시켜주기
         arr.append((x_orign * y_orign) // x)
         
         
-    answer = arr[0]    
+    answer = arr[0]  
+    
     return answer
