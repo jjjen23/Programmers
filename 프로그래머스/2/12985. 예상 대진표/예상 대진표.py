aa6e69ea9c006b80a23ec546eeb8bbbad0c9220d
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-08-22 00:13:52 +0900

    [level 2] Title: 예상 대진표, Time: 53.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,14 +1,13 @@
 def solution(n,a,b):
     answer = 1
-    # MAX = max[A,B]
-    # MIN = min[A,B]
     
     while n > 1:
+        # 해당 라운드에서 A와 B가 붙으면 정답 출력하기
         for i in range(1,n+1,2):
             if (a == i and b == i+1) or (b == i and a == i+1):
                 return answer
         
-        
+        # 해당 라운드에서 붙지 않는다면, 다음 라운드로 gogo..
         answer += 1
         n //= 2
 
