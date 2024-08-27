0a8e66b69fec2f42896494d2e07508496af1d38e
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-08-28 00:55:53 +0900

    [level 2] Title: n^2 배열 자르기, Time: 49.74 ms, Memory: 18.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -0,0 +1,29 @@
+def solution(n, left, right):
+    answer = []
+    # arr2 = [[0]*n for _ in range(n)]
+    #print(arr2)
+    
+    # newarr = []
+    
+    # x or y값 중 더 큰 값이 arr[x][y] 의 값이 된다. (규칙) 
+    # for i in range(n):
+    #     for j in range(n):
+    #         arr2[i][j] = max(i,j) + 1
+    #     newarr += arr2[i]
+    #print(arr2)
+    
+    # newarr = []
+    # for i in range(n):
+    #     newarr += arr2[i]
+    
+#     print(newarr)
+    
+#     answer = newarr[left:right+1]
+
+#     print(answer)
+    for i in range(left, right+1):
+        row = i // n
+        col = i % n
+        answer.append(max(row,col)+1)
+    
+    return answer
