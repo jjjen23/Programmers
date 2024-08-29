2ea272328bc11251f7d444b0d0f66dc04f6b4e5d
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-08-29 21:57:14 +0900

    [level 2] Title: 행렬의 곱셈, Time: 70.64 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -4,7 +4,6 @@ def solution(arr1, arr2):
     answer = []
     for k1 in range(len(arr1)):
         temlist = []
-        # 열로 적용되야하는데 행으로 적용되고 있음
         for k2 in range(len(arr2[0])):
             tem = 0
             for i in range(len(arr1[0])):
