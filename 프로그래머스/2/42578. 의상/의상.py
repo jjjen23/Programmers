d9ee0617216200496e3f30e3c4998e9dcb873c9f
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-08-29 23:02:02 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -2,43 +2,22 @@ from itertools import combinations
 
 def solution(clothes):
     answer = 0
+    
+    # 카테고리별 옷의 수 저장(옷의 종류는 상이하다.)
     fashion  = {}
     for cloth in clothes:
         if cloth[1] not in fashion:
             fashion[cloth[1]] = 1
         else:
             fashion[cloth[1]] += 1
-            
-    # print(fashion)
-    # catelist = list(fashion)
-    # print(catelist)
     
-    # 하나씩 착용하는 경우의 수
-    # answer += sum(fashion.values())
-    
-    # # 조합해서 착용하는 경우의 수
-    # for i in (2,len(fashion)+1):
-    #     #print(i)
-    #     for tuple in combinations(catelist,i):
-    #         #print(tuple)
-    #         tem = 1
-    #         for j in tuple:
-    #             tem *= fashion[j]
-    #         answer += tem        
-    # # print(sum(fashion.values())) # 총 옷들의 수
-    # # print(len(fashion)) # 옷 카테고리는 몇개인지
-    
-    
-    # 하나씩 착용하는 경우의 수
-    #answer += sum(fashion.values())
-    #print(answer)
-    
-    # 조합해서 입는 경우의 수
+    # 조합해서 입는 경우의 수 => a,b,c 조합으로 가능한 모든 경우의 수 : a*b*c
     if len(fashion) > 1:
         tem = 1
         for cnt in fashion.values():
+            # 각각의 카테고리를 안입는 경우의 수 1 추가(ab,ac,bc 같은 경우에 해당)
             tem *= (cnt + 1)
-
+        # 모든 카테코리를 안입는 경우의 수(공집합) 제외 시켜주기
         answer += (tem - 1)
         #print(answer)
         
