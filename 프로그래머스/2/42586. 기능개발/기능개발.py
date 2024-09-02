a9fec8ef31c25499dcbf132dd72276873d2a1d04
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-09-02 23:23:09 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -4,35 +4,24 @@ def solution(progresses, speeds):
     answer = []
     nameunday = []
     
-    #print(math.ceil(3.5))
-    
+    # 작업이 끝나기 까지 남은 일 수 계산해주기
     for i in range(len(progresses)):
         tem = math.ceil((100 - progresses[i]) / speeds[i])
         nameunday.append(tem)
     
     #print(nameunday)
     
-    # while len(nameunday):
-    #     tem = 1
-    #     wait = nameunday.pop(0)
-    #     for i in range(len(nameunday)):
-    #         if wait >= nameunday[i]:
-    #             tem += 1
-    #         else:
-    #             print(i)
-    #             answer.append(tem)
-    #             del nameunday[0:i]
-    #             break
-    
-
+    # 리스트가 비어 있지 않을 때까지 반복
     while len(nameunday):
         m = nameunday.pop(0)
         tem = 1
         
+        # 리스트가 비어있지 않고, 뒷순서가 나보다 작다면 오늘 릴리즈할 기능수 증가시켜주기
         while nameunday and m >= nameunday[0]:
             nameunday.pop(0)
             tem += 1
         
+        # 뒷순서가 크다면 정답에 추가 -> 다시 반복
         answer.append(tem)
     
         
