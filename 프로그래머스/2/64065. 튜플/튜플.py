5ff6a6c4e3489d8378daaf807ba386322269dc6b
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-09-04 23:21:52 +0900

    [level 2] Title: 튜플, Time: 25.96 ms, Memory: 20 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -6,10 +6,13 @@ def solution(s):
     list_2 = [] 
     for i in list_s:
         list_2.append(i.split(","))
+        
+    # print(list_2)
     
     list_2 = sorted(list_2, key = lambda x : len(x))
     
     answer.append(int(list_2[0][0]))
+    
     for i in range(1, len(list_2)): 
         answer.append(int(list(set(list_2[i])-set(list_2[i-1]))[0]))
         
