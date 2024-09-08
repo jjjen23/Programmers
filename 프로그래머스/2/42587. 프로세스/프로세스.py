eaf8d8e8810e872c2c1fb6e5571b3c230cefdfba
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-09-08 23:00:17 +0900

    [level 2] Title: 프로세스, Time: 1.23 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,13 +1,14 @@
 from collections import deque 
 def solution(priorities, location):
     answer = 0
-    # wanted = priorities[location]
-    # print(wanted)
+
     indexlist = list(range(0,len(priorities)))
-    #print(indexlist)
+
     indexlist = deque(indexlist)
     priorities = deque(priorities)
+    
     cnt = 0
+    
     while priorities:
         maxi = max(priorities)
         tem = priorities.popleft()
@@ -21,4 +22,3 @@ def solution(priorities, location):
             priorities.append(tem)
             indexlist.append(idx)
     
-    #return answer
