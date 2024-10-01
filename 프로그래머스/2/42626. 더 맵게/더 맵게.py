d0531ed903e0426c105faecff7452ba84c98ae22
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-10-01 23:24:55 +0900

    [level 2] Title: 더 맵게, Time: 1669.40 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -2,10 +2,11 @@ from heapq import *
 
 def solution(scoville, K):
     
-    heapify(scoville)
+    heapify(scoville) #최소힙으로 변경(기본이 최소힙, 오름차순)
     answer = 0
     mix = 0
     
+    # 두개의 팝이 필요하니까 길이가 2이상일 때만 반복, 오름차순 정렬이니까 가장 작은 값이 K 이상이면 모든 음식의 스코빌 지수가 K이상이니 종료
     while len(scoville) > 1 and scoville[0] < K:
         minValue = heappop(scoville)
         maxValue = heappop(scoville)
@@ -13,8 +14,10 @@ def solution(scoville, K):
         scov = minValue + maxValue*2
         heappush(scoville, scov)
     
+    # 전 부 섞음에도 불구하고 최소값이 K보다 작다면 -1리턴
     if scoville[0] < K:
         answer = -1
+    # 섞은 횟수 출력
     else:
         answer = mix
     
