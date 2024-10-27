76a2505afa2a50ba3b4ff0de2f5490d2965d0995
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-10-27 22:36:29 +0900

    [level 2] Title: 가장 큰 수, Time: 169.69 ms, Memory: 21 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -10,14 +10,14 @@ def solution(numbers):
     for i in sorted_num:
         answer += str(i)
 
-# 미스테리 미스테리.. 오.. 왜 아래는 안돼..
+# 미스테리 미스테리.. 오.. 왜 아래는 안돼.. -> 딕셔너리는 똑같은 값에 대해 처리불가능..!!!! ex)[0, 1000, 0, 0]의 입력의 경우, 1000000가 아니라, 10000가 나옴
 #     dict1 = {}
 #     for num in numbers:
 #         dict1[num] = (str(num)*4) # 1000자리 수 까지 확장해서, 비교.  
     
 #     dict2 = dict(sorted(dict1.items(), key= lambda x: x[1], reverse = True))
     
-#     # print(dict1)
+#     # print(dict2)
     
 #     for item in dict2.items():
 #         answer+=str(item[0])
