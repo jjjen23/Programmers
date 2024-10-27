926a975adf83e36c3edf09a57d0245aa45af6aef
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2024-10-27 21:40:52 +0900

    [level 2] Title: 가장 큰 수, Time: 109.43 ms, Memory: 20.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -10,15 +10,16 @@ def solution(numbers):
     for i in sorted_num:
         answer += str(i)
 
+# 미스테리 미스테리.. 오.. 왜 아래는 안돼..
 #     dict1 = {}
 #     for num in numbers:
 #         dict1[num] = (str(num)*4) # 1000자리 수 까지 확장해서, 비교.  
     
-#     dict1 = dict(sorted(dict1.items(), key= lambda x: x[1], reverse = True))
+#     dict2 = dict(sorted(dict1.items(), key= lambda x: x[1], reverse = True))
     
 #     # print(dict1)
     
-#     for item in dict1.items():
+#     for item in dict2.items():
 #         answer+=str(item[0])
 #     # print(answer)    
     
