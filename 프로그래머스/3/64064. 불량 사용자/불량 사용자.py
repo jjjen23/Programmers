6bbf5540cd83108715cf4f7f6bc4b0a162eb5adb
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2025-01-28 00:15:21 +0900

    [level 3] Title: 불량 사용자, Time: 5846.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -18,7 +18,7 @@ def solution(user_id, banned_id):
         candidate.append(temp)
     print(candidate)
     
-    res =set()
+    res = set()
     
     for p in product(*candidate):
         if len(set(p)) == len(banned_id):
@@ -26,41 +26,6 @@ def solution(user_id, banned_id):
     print(res)
                 
     answer = len(res)
-    
-#     new_ban = []
-    
-#     total = []
-#     dict1 = {}
-#     # 제재 아이디 목록은 몇가지 경우의 수가 가능한지 리턴하시오!
-#     # * ->를 .으로 변경
-#     for id in banned_id:
-#         tem = id.replace('*','.')
-#         dict1[tem] = set()
-#         new_ban.append(tem)
-        
-#     # print(dict1)
-    
-#     for banid in new_ban:
-#         for userid in user_id:
-#             if re.fullmatch(banid, userid):
-#                 dict1[banid].add(userid)
-                
-#     print(dict1)
-    
-    
-    
-#     # print(new_ban)
-    
-#     setban = set(new_ban)
-#     totalcom = []
-    
-#     for ban in setban:
-#         cnt = new_ban.count(ban)
-#         totalcom.append(list(combinations(dict1[ban],cnt)))
-    
-#     print(totalcom)
-    
-#     for i in totalcom:
-#         answer *= len(i)
+
     
     return answer
