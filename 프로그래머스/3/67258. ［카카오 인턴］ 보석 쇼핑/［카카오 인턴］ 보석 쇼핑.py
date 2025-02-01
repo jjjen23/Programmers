9491e3d4f9e4efd28ad3543cfbb6c26a346b4b57
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2025-02-01 15:50:41 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 62.22 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,23 +1,32 @@
+# 문제 이해
+# 모든 보석을 하나 이상 포함하는 가장 짧은 구간을 찾아서 구간 인덱스 번호+1 를 반환하라
+# 만일 짧은 구간이 하나 이상 -> 시작 진열대 번호가 가장 작은 구간 반환!!
+# 우선순위 1. 최소길이 2. 시작 인덱스가 작은 것 
+
 def solution(gems):
-    best = [0,len(gems)]
     
-    # 문제 이해
-    # 모든 보석을 하나 이상 포함하는 가장 짧은 구간을 찾아서 구간 인덱스 번호+1 를 반환하라
-    # 만일 짧은 구간이 하나 이상 -> 시작 진열대 번호가 가장 작은 구간 반환!!
+    # 최악의 경우로 초기화 = 모든 요소 포함하는 경우
+    best = [0,len(gems)]
     setgems = len(set(gems))
     
     gemdict = {}
     left , right = 0,0
     
     while right < len(gems):
+        # 딕셔너리에 있다면 +1
         if gems[right] in gemdict:
             gemdict[gems[right]] += 1
+        # 없다면 생성, 1로 초기화
         else:
             gemdict[gems[right]] = 1
-            
+        
+        # 오른쪽 인덱스 이동
         right += 1
         
+        # 모든 종류의 보석을 만족한다면 left를 이동하며 만족하는 최소 길이를 탐색
         while len(gemdict) == setgems:
+            # 더 짧은 구간 인덱스로 갱신
+            # 작을 때에 대해서만 갱신하므로 무조건 최소 인덱스로 갱신함(순차 탐색)
             if right - left < best[1] - best[0]:
                 best = [left, right]
             
@@ -67,6 +76,6 @@ def solution(gems):
     return answer
 
 
-2. 딕셔너리, 투포인터 이용 -> set(gems[i:j]) 부분을 해결해야함, 이를 딕셔너리로 
+2. 딕셔너리, 투포인터 이용 -> set(gems[i:j]) 부분을 해결해야함: -> 구현 못해서 답지 참고, 투 포인터 사용이 익숙하지 않았고 딕셔너리를 이렇게 활용 할 생각은 못했다. 또한 최악의 경우로 초기화하는 방법도 생각 못함 항상 최악의 경우로 초기화해서 갱신하는 습관이 필요해보임!!
 
 """
