4b109ec044165f9bc512ce7e179dcfab7601c71a
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2025-02-03 13:35:25 +0900

    [level 3] Title: 가장 먼 노드, Time: 79.35 ms, Memory: 26.6 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -48,3 +48,25 @@ def solution(n, edge):
     
     
     return answer
+
+"""
+    review: 문제풀면서 아쉬웠던 것: 모든 간선 길이가 동일한데, 저렇게 1로 넣어주는거 말고 더 합리적인 방법이 있을까?
+        
+        # 길이는 저장하지 않기
+        for e in edge:
+        a,b = e
+        graph[a].append(b)
+        graph[b].append(a)
+        
+        for i in graph[now]:
+            cost = dist + 1 # 모든 간선 길이는 1
+            if cost < distance[i]:
+                distance[i] = cost
+                heapq.heappush(queue,(cost,i))
+        
+        이렇게 하면 된다.
+        
+        나는 다익스트라로 풀었는데 bfs로도 푸는 방법도 있다.
+        생각해보니까 그렇네.. 다익스트라 연습도 할 겸..
+
+"""
