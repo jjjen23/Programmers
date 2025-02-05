9d333410d83edf59c1c44b8566dc7a92741b42d0
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2025-02-05 14:33:31 +0900

    [level 3] Title: 부대복귀, Time: 1008.82 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.py
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.py
@@ -18,8 +18,8 @@ def solution(n, roads, sources, destination):
         graph[x].append(y)
         graph[y].append(x)
     
-    # 출발지 -> 목적지 최단길이 찾아주는 다익스트라 알고리즘 설계 -> 시간초과
-    # 개선 : 목적지 -> 출발지 다익스트라 실행시 한번만 다익스트라 수행하면됨;;;
+    # 출발지 -> 목적지 최단길이 찾아주는 다익스트라 알고리즘 설계 -> 출발지별 다익스트라 다회 수행 -> 시간초과
+    # 개선 : 목적지 -> 출발지 다익스트라 실행시 한번만 다익스트라 수행하면 됨;;;
     
     def dijkstra(start):
         q = []
@@ -38,15 +38,20 @@ def solution(n, roads, sources, destination):
                     distance[i] = cost
                     heapq.heappush(q,(cost,i))
     
-    # 다익스트라수행
+    # 목적지 출발 다익스트라 수행
     dijkstra(destination)
     
-    # 출발지, 목적지별 최단경로를 추가해준다.
+    # 목적지별 최단경로를 추가해준다.
     for s in sources:
         if distance[s] != INF:
             answer.append(distance[s])
+        # 경로가 없다면 -1 추가
         else:
             answer.append(-1)
     
     
     return answer
+
+"""
+    review: 다익스트라 알고리즘 문제라고 생각했다. 처음에 출발지별로 다익스트라를 전부 돌렸는데 시간초과가 났다. .. 당연하다 사실상 목적지가 전부 동일하기 때문에 목적지로부터 다익스트라를 수행하면 한번만 다익스트라 알고리즘을 수행해서 원하는 결과를 얻을 수 있다. 이렇게 변경하니 바로 문제가 풀렸다. 이제 다익스트라 알고리즘은 좀 익숙해진 것 같다...!!!!!
+"""
