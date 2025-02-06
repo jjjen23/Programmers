964a78f60bc532cecc6c8d51583ebb4031286524
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2025-02-06 23:30:24 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 501.42 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -3,16 +3,14 @@ def solution(sequence):
     
     # 연속 펄스(-1 , 1 , -1 or 그 반대 부호) * 부분 수열의 합이 가장 큰 것을 출력하라.
     # 일단 넣어봐 숫자 그 다음에 뺐을때 더 크면 빼..  -> 투포인터 이용해보려고 했는데 잘 안됨 
-    # 어떤 펄스든 결론적으로 두 펄스의 절대값은 동일함 -> 한 펄스만 구해서 양수화하면됨
     # 답지참고 -> dp활용
-    # min(현재, 현재+)
     
     size = len(sequence)
     s1 = []
     s2 = []
     pulse = 1
 
-    
+    # 펄스별 sequence 재생성
     for i in range(size):
         s1.append(sequence[i] * pulse)
         s2.append(sequence[i] * (-pulse))
@@ -26,13 +24,16 @@ def solution(sequence):
     
     
     # 현재랑, 이전 값+현재랑 더 큰것을 다음 dp에 갱신
+    # 현재 선택 -> 앞에 다 버려 너부터 다시 시작.
+    # 누적 선택 -> 계속해서 누적해감.
     for i in range(1, size):
         dp1.append(max(s1[i], dp1[i-1]+s1[i]))
         dp2.append(max(s2[i], dp2[i-1]+s2[i]))
     
-    # print(dp1)
-    # print(dp2)
+    # print(dp1) # <- 각 인덱스 위치 별 최적 누적합을 계산한 것
+    # print(dp2) # <- 각 인덱스 위치 별 최적 누적합을 계산한 것
     
+    # 각 펄스별 최고값 골라서 그 중 최고 출력!!!
     answer = max(max(dp1),max(dp2))
     
 
