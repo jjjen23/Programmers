cd4865d28c25e37ae8a4bba22cc5c8fdb2cafac7
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2025-02-07 23:23:44 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 249.57 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -10,6 +10,7 @@ def solution(s):
     
     maxLen = 1 # 최소 한 글자는 항상 팰린드롬임!!
     
+    # 해설참고
     # 하나의 중심에서 확장 -> 팰린드롬 길이 홀수
     # 두개의 중심에서 확장 -> 팰린드롬 길이 짝수
     
@@ -22,7 +23,7 @@ def solution(s):
     
     maxlen = 1 # 최소 팰린드롬의 길이는 1
     
-    for i in range(0,len(s)):
+    for i in range(len(s)):
         odd = center(i,i)
         even = center(i,i+1) 
         maxLen = max(maxLen, even, odd)
@@ -34,7 +35,9 @@ def solution(s):
 
 
 """
-def solution(s):
+ review: 하나의 인덱스를 중심으로 범위를 넓혀가는 접근 방식은 맞았다. 근데 짝수길이 팰린드롬 찾는법은 생각하지 못해서 답지를 참고
+ 
+ 방법1 : def solution(s):
     answer = 0
     
     # 투 포인터로 해볼게
@@ -45,32 +48,39 @@ def solution(s):
     # 실패한 이유2 : 대칭이 전부 홀수일것이라고 가정한 나."abba"처럼 짝수일 수 있다. , 원소가 하나인 경우에 대해 고려안함.
     
     maxLen = 1 # 최소 한 글자는 항상 팰린드롬임!!
-    s = list(s)
     
-    for i in range(1,len(s)):
-        tem = s[:i+1]
-        tem = tem[::-1]
-        # print(tem)
-        j = i
-        if tem == s[i:i+i+1]:
-            # 갱신
-            maxLen = max(maxLen, (i*2+1))
-        else:
-            # i가 1일때까지 i를 1씩 빼서 비교한다.
-            while j > 1 :
-                j -= 1
-                tem = s[i-j:i+1]
-                tem = tem[::-1]
-                # print("tem",tem)
-                # print("other",s[i:i+j+1])
-                if tem == s[i:i+j+1]:
-                    # 갱신
-                    maxLen = max(maxLen, (j*2+1))
-                    break
-                
-                
-    answer = maxLen    
+    # 해설참고
+    # 하나의 중심에서 확장 -> 팰린드롬 길이 홀수
+    # 두개의 중심에서 확장 -> 팰린드롬 길이 짝수
     
+    def center(left,right):
+        # 인덱스 범위 내이고, 오,왼이 같다면 1씩 늘려
+        while left >=0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1
+    
+    maxlen = 1 # 최소 팰린드롬의 길이는 1
+    
+    for i in range(len(s)):
+        odd = center(i,i)
+        even = center(i,i+1) 
+        maxLen = max(maxLen, even, odd)
+        
+    answer = maxLen
 
     return answer
+    
+    방법2(완전탐색) -> 효율성 시간초과:
+    def solution(s):
+        answer = 1 # 최소 한 글자는 항상 팰린드롬임!!
+
+        for i in range(0,len(s)):
+            for j in range(1, len(s)+1):
+                if s[i:j] == s[i:j][::-1]:
+                    answer = max(answer,len(s[i:j]))
+
+    return answer
+    
+
 """
