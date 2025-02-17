0f9b18587ac8d85506746662e44978127c5ad506
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2025-02-17 11:37:56 +0900

    [level 3] Title: 인사고과, Time: 188.84 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.py
+++ 프로그래머스/3/152995. 인사고과/인사고과.py
@@ -11,43 +11,33 @@
 # 2. 인센티브를 받는 자들의 석차는 -> (인덱스, 합) 리스트 추가 후 정렬?
 def solution(scores):
     answer = 0
-    target_a, target_b = scores[0]
-    target_score = target_a + target_b
 
-    # 첫번째 점수에 대해서 내림차순,
-    # 첫 번째 점수가 같으면 두 번째 점수에 대해서 오름차순으로 정렬합니다.
-    scores.sort(key=lambda x: (-x[0], x[1]))
+    target_a, target_b = scores[0] # 완호 점수
+    target_score = target_a + target_b # 완호 점수 합
+    
+    # 근무태도는 내림차순, 동료평가는 오름차순으로 정렬한다.
+    scores.sort(key = lambda x : (-x[0],x[1]))
     maxb = 0
     
     for a, b in scores:
+        # 완호가 인센티브를 받지 못하는 경우(한 번이라도 둘 다 작은 경우)
         if target_a < a and target_b < b:
             return -1
-        
+        # a는 무조건 다음이 작으니, b만 확인해주면 된다.
+        # 갱신된 maxb보다 작으면, 인센티브 제외 -> 인센티브 받는 사람 골라내는 작업
         if b >= maxb:
             maxb = b
+            # 인센티브 받는 건에 대해선 원호보다 점수가 큰 경우만 확인하면된다. 
             if a + b > target_score:
+                # 완호 등수가 밀림(클때만 밀리면 동석차로 계산된)
                 answer += 1
-#     target_a, target_b = scores[0] # 완호 점수
-#     target_score = target_a + target_b # 완호 점수 합
-    
-#     # 근무태도는 내림차순, 동료평가는 오름차순으로 정렬한다.
-#     scores.sort(key = lambda x : (-x[0],x[1]))
-    
-#     maxb = 0
-    
-#     for a, b in scores:
-#         # 완호가 인센티브를 받지 못하는 경우(한 번이라도 둘 다 작은 경우)
-#         if target_a < a and target_b < b:
-#             return -1
-#         # a는 무조건 다음이 작으니, b만 확인해주면 된다.
-#         # 갱신된 maxb보다 작으면, 인센티브 제외 -> 인센티브 받는 사람 골라내는 작업
-#         if b >= maxb:
-#             mavb = b
-#             # 인센티브 받는 건에 대해선 원호보다 점수가 큰 경우만 확인하면된다. 
-#             if a + b > target_score:
-#                 # 완호 등수가 밀림(클때만 밀리면 동석차로 계산된)
-#                 answer += 1
     
     
     
     return answer+1
+
+"""
+    review: 전체탐색을 하지않고 어떻게 순위를 결정할 수 있을지 생각이 전혀 안나서 정답 코드 참고 -> 내가 이렇게 잘 활용할 수 있을까.. 대박이다. ㅡ .. ㅡ
+
+"""
+
