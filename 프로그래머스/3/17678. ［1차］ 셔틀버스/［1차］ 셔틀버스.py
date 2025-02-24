e639b48e32ae5b09bf26aea6c60b3857882c2638
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2025-02-24 17:05:26 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 0.87 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
@@ -14,40 +14,72 @@
 def solution(n, t, m, timetable):
     answer = ''
     now = 9*60
-    end = 23*60 + 59
     
-    
+    # 비교를 위해 시간 형식 변경
     for i in range(len(timetable)):
         H,M = timetable[i].split(':')
         timetable[i] = int(H) * 60 + int(M)
     
-    timetable.sort() # 오름차순정렬
+    timetable.sort(reverse=True) # 오름차순정렬
     
-    # 종료조건 : 운행 수
     while n > 0:
         n -= 1
         cnt = 0
         
-        while timetable and timetable[0] <= now and cnt < m:
-            last = timetable.pop(0)
+        # 해당 배차에 조건에 맞는 애들만큼 탑승시켜
+        while timetable and timetable[-1] <= now and cnt < m:
+            last = timetable.pop()
             cnt += 1
+            
         
-        if n == 0: # 마지막 버스일 때
-            if cnt < m :
-                # 자리가 남았다면 출발시각에 도착
-                answer = now
+        # 마지막 버스라면
+        if n == 0:
+            # 마지막 버스에 좌석이 있다면
+            if cnt < m:
+                answer = now # 마지막 버스 출발시각이 가장 늦음
             else:
-                # 자리가 없다면 마지막 탄 사람보다 1분 일찍 도착
-                answer = last - 1
+                # 마지막 버스 좌석 여유가 없다면
+                answer = last - 1 # 리얼 마지막 탑승하는 애 앞으로 가
+    
+        # 다음 버스 탑승시각으로
         else:
             now += t
+
         
                 
                 
     # print(now)
     # print(timetable)
     
-    # 시간형식 변환
+    # 시간형식 재변환
     answer = f"{answer//60:02d}:{answer%60:02d}"
     
     return answer
+
+"""
+ review : 주춤한 것 치고 알고리즘은 잘짬 근데. # 여유가 없다면 -> 초과하는 인원만큼 n분 빨리 탑승가 아니라 그냥 마지막에 탈사람보다 -1 분 하면 간단할 일!!! .. 핵심은 마지막 버스일때 결정..!!
+"""
+
+"""
+    # 종료조건 : 운행 수
+    while n > 0:
+        n -= 1
+        cnt = 0
+        
+        # 정원(m)까지 조건에 해당하면 pop or 정원미달이여도 인원만큼 pop
+        while timetable and timetable[0] <= now and cnt < m:
+            last = timetable.pop(0)
+            cnt += 1
+        
+        if n == 0: # 마지막 버스일 때
+            if cnt < m :
+                # 자리가 남았다면 출발시각에 도착(완저니. 막차다.)
+                answer = now
+            else:
+                # 자리가 없다면 마지막 탄 사람보다 1분 일찍 도착
+                answer = last - 1
+                
+        # 마지막 버스 아니면 무조건 다음 배차로 
+        else:
+            now += t
+"""
