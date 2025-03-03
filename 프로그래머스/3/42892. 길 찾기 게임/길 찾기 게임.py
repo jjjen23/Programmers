e948c0ca9656951877516fce8baaf3ef4f9b620c
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2025-03-03 23:16:03 +0900

    [level 3] Title: 길 찾기 게임, Time: 351.89 ms, Memory: 14.9 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -9,12 +9,29 @@ setrecursionlimit(10000) #기존 1000으로 제한된 재귀 깊이를 늘려줌
 
 class Node(object):
     def __init__(self,info):
-        self.num = info[2]
-        self.pos = info[:2]
-        self.left = None
+        self.num = info[2] # 노드(idx) 번호 저장
+        self.pos = info # [x,y,idx]
+        self.left = None 
         self.right = None
 
-
+# parent == tree 
+# info == 신입
+# add_node 함수 == 트리의 루트부터 탐색하며 조건에 맞는 신입 자리 찾아주는 함수
+def add_node(parent,info):
+    # 부모 보다 x값이 작다면 left으로
+    if parent.pos[0] > info[0]:
+        # 자식이 존재하면 재귀 호출, 자식이 없는 곳 까지 깊이 탐색
+        if parent.left:
+            add_node(parent.left,info)
+        # None이라면, 새로운 노드를 생성하여 부모의 자식으로 설정
+        else:
+            parent.left = Node(info)
+    # 부모보다 x값이 크다면 right으로
+    else:
+        if parent.right:
+            add_node(parent.right,info)
+        else:
+            parent.right = Node(info)
 
 
 def solution(nodeinfo):
@@ -23,28 +40,19 @@ def solution(nodeinfo):
         # [x,y,idx]가 들어가게 인덱스 정보 추가
         nodeinfo[i].append(i+1)
         
-    # 왼쪽은 자신보다 y좌표가 작은 노드가 오른쪽은 자신보다 y좌표가 큰 노드가 들어가게 구성
+    # 왼쪽은 자신보다 y좌표가 작은 노드가 오른쪽은 자신보다 y좌표가 큰 노드가 들어가게 구성 -> 부모노드가 먼저 나오도록 정렬해줌. -> 여기까진 사고함(?)
     nodeinfo.sort(key = lambda x : (-x[1],x[0]))
-    tree = Node(nodeinfo[0])
     
+    tree = Node(nodeinfo[0]) # 트리생성,루트생성
+    
+    # 생성된 루트에 add_node함수를 통해서 노드를 추가해나감
     for info in nodeinfo[1:]:
         add_node(tree,info)
         
     
     return [pre_order(tree),post_order(tree)]
 
-def add_node(parent,info):
-    # 부모보다 x값이 작다면 left 자식
-    if parent.pos[0] > info[0]:
-        if parent.left:
-            add_node(parent.left,info)
-        else:
-            parent.left = Node(info)
-    else:
-        if parent.right:
-            add_node(parent.right,info)
-        else:
-            parent.right = Node(info)
+
             
 # 부모 -> Left -> Right 순서로
 def pre_order(curr):
