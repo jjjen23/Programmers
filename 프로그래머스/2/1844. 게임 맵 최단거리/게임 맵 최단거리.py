914692e50b6738a612f0d7f68a12edb0017b3d10
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2025-06-04 23:56:51 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.24 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -6,6 +6,50 @@ def solution(maps):
     dx = [0,0,1,-1]
     dy = [1,-1,0,0]
     
+    col = len(maps[0])
+    row = len(maps)
+    
+    visited = [[0]*col for i in range(row)]
+    # print(visited)
+    
+    
+    def bfs(x,y):
+        q = deque([(x,y)])
+        visited[x][y] = 1
+    
+        while q:
+            x, y = q.popleft()
+
+            
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                
+                if 0<= nx< row and 0<= ny < col and visited[nx][ny] == 0 and maps[nx][ny]==1:
+                    q.append((nx,ny))
+                    visited[nx][ny] = visited[x][y] + 1
+
+        if visited[row-1][col-1] != 0:
+            return visited[row-1][col-1]
+        else:
+            return -1
+    
+    answer = bfs(0,0)
+                    
+    
+    return answer
+
+
+
+"""
+from collections import deque
+
+def solution(maps):
+    answer = 0
+    
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
+    
     
     visited = [[0]*len(maps[0]) for _ in range(len(maps))]
     #print(vistited)
@@ -35,3 +79,4 @@ def solution(maps):
         
     
     return answer
+"""
