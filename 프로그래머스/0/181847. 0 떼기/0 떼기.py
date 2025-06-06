3365b76fd8fb07442be0df2576d26df4967b0f74
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2025-06-06 21:41:36 +0900

    [level 0] Title: 0 떼기, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.py
@@ -0,0 +1,13 @@
+def solution(n_str):
+    answer = ''
+    
+    i = 0
+    while True:
+        if n_str[i] == '0':
+            i+=1
+            continue
+        else:
+            break
+            
+    answer = n_str[i:]
+    return answer
