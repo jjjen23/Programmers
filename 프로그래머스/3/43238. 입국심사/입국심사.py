264ceda3345881fe2d811866e0d486857199c18a
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2025-06-07 22:39:41 +0900

    [level 3] Title: 입국심사, Time: 538.27 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,6 +1,32 @@
 def solution(n, times):
     answer = 0
     
+    # 모든 사람이 심사받는데 걸리는 최소 시간 반환
+    Min = min(times)
+    Max = n * max(times)
+    
+    # 모든사람을검사하는데 걸리는 총시간에 각 심사관의 심사시간을 나누면 total시간에 심사관당 몇명을 검사할 수 있는지 알 수 있다. -> 총 시간을 이진탐색으로 조정하면서 n명을 모두 검사할 수 있는 최소 시간을 찾는다!!!
+    
+    while Min <= Max:
+        Mid = (Min + Max) // 2
+        timepern = 0
+        for i in times:
+            timepern += Mid // i
+        
+        if timepern >= n:
+            answer = Mid
+            Max = Mid - 1
+        else:
+            Min = Mid + 1
+    
+    
+    return answer
+
+
+"""
+def solution(n, times):
+    answer = 0
+    
     left = min(times) # 가능한 최소 시간
     right = max(times)*n # 인원수 만큼 가능한 최대
     
@@ -28,3 +54,5 @@ def solution(n, times):
             left = mid + 1
     
     return answer
+
+"""
