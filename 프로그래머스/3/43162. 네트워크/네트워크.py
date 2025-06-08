32207554b96273eac7adbe07365efda86d7e0d70
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2025-06-08 22:42:19 +0900

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -2,6 +2,31 @@ from collections import deque
 
 def solution(n, computers):
     answer = 0
+    visited = [0] * n #노드 수만큼 방문 체크 배열 생성
+    
+    def bfs(node):
+        q = deque([node])
+        visited[node] = 1
+        
+        while q:
+            nnode = q.popleft()
+            for i in range(n):
+                if computers[nnode][i] == 1 and visited[i] != 1:
+                    visited[i] = 1
+                    q.append(i)
+    
+    for i in range(n):
+        if not visited[i]:
+            bfs(i)
+            answer += 1
+    
+    return answer
+
+"""
+from collections import deque
+
+def solution(n, computers):
+    answer = 0
     visited = [0]*n
     # print(visited)
     # dx = [0,0,1,-1]
@@ -16,13 +41,16 @@ def solution(n, computers):
             current = dq.popleft()
             
             for i in range(n):
+                # 특정 노드와 연결되어 있고, 방문하지 않았다면 -> 방문추가, 큐에 추가 
                 if computers[current][i] == 1 and not visited[i]:
                     visited[i] = 1
                     dq.append(i)
 
     for i in range(n):
+        # 방문안된곳 bfs 돌기 -> bfs도는 만큼이 네트워크 수
         if not visited[i]:
             bfs(i)
             answer += 1
     
     return answer
+"""
