65ea4fab60a664a60bbdfda49803cedd843af29d
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2025-09-24 11:26:32 +0900

    [Gold IV] Title: 연구소, Time: 1932 ms, Memory: 36616 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,62 +1,64 @@
+n,m = map(int, input().split())
+
+# 관건은 어디에 벽을 3개 세울 것인지 -> 전부 탐색
+# 안전영역의 개수가 가장 커지도록 벽을 세워야하고, 그때의 안전영역 수를 출력
+# 0은 빈 칸, 1은 벽, 2는 바이러스
+
 from itertools import combinations
 from collections import deque
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
 
-# bfs로 바이러스 전파
-def bfs(x,y, temp) :
+def bfs(x,y,tem):
     q = deque([(x,y)])
 
-    while q :
-        cx, cy = q.popleft()
-        for i in range (4) :
-            nx, ny = cx + dx[i], cy + dy[i]
-            if nx < 0 or nx > n-1 or ny < 0  or ny > m-1 or temp[nx][ny] == 1 or temp[nx][ny] == 2:
-                continue
-            else : 
-                temp[nx][ny] = 2 
-                q.append((nx, ny))
+    while q:
+        cx,cy = q.popleft()
+        for i in range(4):
+            nx,ny = cx+dx[i], cy+dy[i]
+            if 0<=nx<n and 0<=ny<m and tem[nx][ny] == 0:
+                tem[nx][ny] = 2
+                q.append((nx,ny))
 
+safe, virus, lab = [], [], []
+combs = []
+temp = [[0]*m for i in range(n)]
 
-n, m = map(int, input().split())
-lab, safe, virus = [], [], []
-temp = [[0] * m for _ in range(n)] # 매번 달라지는 map을 처리해주는 행렬 초기화
-results = []
-
-for x in range(n) :
+for i in range(n):
     data = list(map(int, input().split()))
-    for y in range (m) :
-        if data[y] == 0 :
-            safe.append((x,y))
-        elif data[y] == 2:
-            virus.append((x,y))
+    for j in range(m):
+        if data[j] == 0:
+            safe.append((i,j))
+        elif data[j] == 2:
+            virus.append((i,j))
     lab.append(data)
 
-walls = []
-# 가능한 조합 검사
-for i in combinations(safe, 3):
-    walls.append(i)
+for i in combinations(safe,3):
+    combs.append(i)
 
-for wall in walls :
-    safecnts = 0 # 0의 개수
-    # 맵 정보 복사
-    for i in range (n) :
-        for j in range(m) :
-            temp[i][j] = lab[i][j] # temp = lab은 안된다.
-    #  가능한 조합을 순회하며 벽 설치
-    for w in range(3) :
-        wx, wy = wall[w][0], wall[w][1]
-        temp[wx][wy] = 1
-    for v in virus :
-        vx, vy = v[0], v[1]
-        bfs(vx, vy, temp) # 바이러스 퍼트리기
-    # 매 조합마다 안전영역(0)의 개수 세서 정보 저장
-    for i in range (n) :
+res = []
+for comb in combs:
+    # 기존 연구실 구조 복사뜨기
+    for i in range(n):
         for j in range(m):
-            if temp[i][j] == 0 :
-                safecnts+=1
-    results.append(safecnts)
+            temp[i][j] = lab[i][j]
 
-# 가장 큰 값 출력
-print(max(results))
+    for i in range(3):
+        tx,ty = comb[i][0], comb[i][1]
+        temp[tx][ty] = 1
+
+    # 바이러스퍼트리기
+    for i in virus:
+        bfs(i[0],i[1],temp)
+
+    safecnt = 0
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] == 0:
+               safecnt += 1
+    res.append(safecnt)
+
+print(max(res))
+
+
