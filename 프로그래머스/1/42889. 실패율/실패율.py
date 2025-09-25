8cfeb9ddd7f8d76a0deae69ef24ae971163196fa
jjjen23 <115393914+jjjen23@users.noreply.github.com>
2025-09-25 13:31:25 +0900

    [level 1] Title: 실패율, Time: 6920.53 ms, Memory: 17.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,4 +1,53 @@
 def solution(N, stages):
+    answer = []
+    # 실패율을 구하는 코드를 완성하라
+    # 스테이지에 도달 했지만 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
+    # 이용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열 stages
+    # 스테이지 개수 N
+    # 실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열을 반환하라
+    dict1 = {}
+    for i in range(1,N+1):
+        dict1[i] = 0
+    
+    
+
+    for i in range(1,N+1):
+        # i 번 스테이지의 실패율을 구하자
+        arrived = 0
+        notclear = 0
+        
+        for s in stages:
+            if s >= i :
+                arrived += 1
+            if i == s:
+                notclear +=1 
+        # print(arrived,notclear)
+        if arrived == 0:
+            dict1[i] = 0.0
+        else:
+            dict1[i] = notclear/arrived
+    # print(tem)
+    temdict = sorted(list(dict1.items()),key = lambda x : (-x[1],x[0]))
+    for i in range(N):
+        answer.append(temdict[i][0])
+    
+    # 실패율이 높은 스테이지 부터 내림차순으로 스테이지의 번호가 담겨있는 배열을 반환하라(만일 실패율이 같다면 번호가 작은게 먼저)
+    return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+def solution(N, stages):
     rates = []
     answer = []
     user = len(stages)
@@ -16,9 +65,15 @@ def solution(N, stages):
     for i in range(len(rates)):
         answerDict[i+1] = rates[i]
     
-    items = sorted(answerDict.items(), key = lambda x : x[1], reverse=True)
+    items = sorted(answerDict.items(), key = lambda x : -x[1])
     
     for item in items:
         answer.append(item[0])
         
     return answer
+
+
+"""
+
+
+
